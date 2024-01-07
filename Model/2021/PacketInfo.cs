cadb905482516ea1db809d59b108dffc24a47da7
Seth Nielsen <seth.emanuel.nielsen@gmail.com>
2024-01-07 15:35:21 +0100

    push

---
+++ Model/2021/PacketInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2023.Model._2021
+{
+    public class PacketInfo
+    {
+        public int version;
+        public int type;
+        public int value;
+    }
+}
